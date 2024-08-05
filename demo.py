7322221d9df1f0e8f5b18c7bd17636b52fe1d7a0
majo-fflores <mariajosefflores@gmail.com>
2024-08-05 12:35:49 -0300

    lpm

--- demo.py
+++ demo.py
@@ -2,9 +2,5 @@ print('hola mundo')
 
 #comentarios agregados  
 
-<<<<<<< HEAD
 print('chau')
 print('change es cambio Miguel')
-=======
-print('chu')
->>>>>>> 8a896ee0665f80707434e9278c589e7046e942c0
